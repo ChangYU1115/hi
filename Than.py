d3f900e689cd033eb3fb55e8ba66d45a6d727d38
Chang Yu <a0952756905@gmail.com>
2020-11-14 15:46:40 +0800

    first commit

---
+++ Than.py
@@ -0,0 +1,12 @@
+#比較
+
+x = 5
+
+print("x=",x)
+print("===========")
+print("X等於5 :",x == 5)
+print("X不是等於5 :",x != 5)   
+print("X大於5 :",x > 5)
+print("X小於5 :",x < 5)
+print("X大於等於5 :",x >= 5)
+print("X小於等於5 :",x <= 5)
